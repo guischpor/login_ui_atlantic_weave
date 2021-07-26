2fc87bb1b11ba5770cf95248407720e95e0275cb
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2021-07-25 21:25:53 -0300

    Feature(login) Featuare Constants Styles

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+const Color COLOR_BUTTON_BLUE = Color(0xff175B9F);
+const Color COLOR_BACKGROUND_INPUTFIELD = Color.fromRGBO(18, 18, 18, 6);
+const Color COLOR_BORDER_INPUTFIELD = Color.fromRGBO(87, 87, 87, 6);
+const Color COLOR_ICON_INPUTFIELD = Color.fromRGBO(255, 255, 255, 7);
+const Color COLOR_TEXT_INPUTFIELD = Color(0xffA2A2A2);
+const Color COLOR_TEXT_BUTTON = Color(0xffA2A2A2);
+const Color COLOR_TEXT_NEW_ACCOUNT = Color(0xffF7F7F7);
+
+const Gradient BACKGROUND_GRADIENTE_SCREEN = LinearGradient(
+    begin: Alignment.bottomCenter,
+    end: Alignment.center,
+    colors: [
+      Color.fromRGBO(84, 84, 84, 0),
+      Color.fromRGBO(0, 0, 0, 6),
+    ]);
+
+const TextStyle titleLogin = TextStyle(
+  fontSize: 16.0,
+  color: Colors.white,
+  fontWeight: FontWeight.w400,
+);
